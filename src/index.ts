f4b8808e6a9266ad11ea66582b8ccecf2f52ebd5
ulrichsantos <ulrich_sk8@hotmail.com>
2023-04-24 18:14:35 -0300

    criando banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+const mysql = require('mysql');
+
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
@@ -11,4 +13,27 @@ app.get('/', (req: Request, res: Response) => {
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
+});
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '@Dartveider123',
+  database: 'teste_server_carros'
+})
+
+connection.connect((error: { stack: string; }) => {
+  if (error) {
+    console.error('Erro ao conectar ao banco de dados: ' + error.stack);
+    return;
+  }
+  console.log('Conexão bem sucedida com o banco de dados.');
+});
+
+connection.query('SELECT * FROM tabela', (error: { stack: string; }, results: any, fields: any) => {
+  if (error) {
+    console.error('Erro ao executar a consulta: ' + error.stack);
+    return;
+  }
+  console.log('Resultado da consulta: ', results);
 });
